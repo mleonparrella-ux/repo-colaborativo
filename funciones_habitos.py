4df678429fc7e9412409042e6f576ca1b32f7ca8
mleonparrella-ux <mleonparrella@udesa.edu.ar>
2026-03-12 14:23:00 -0300

    Segundo commit
    Hicimos el código entero de los dos archivos, donde se registran las actividades del día.

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -5,3 +5,21 @@ Created on Thu Mar 12 13:50:11 2026
 @author: ramon
 """
 
+def registar_habitos():
+    lista_actividades = []
+    seguir = input("Desea ingresar actividad ")
+    while seguir == "si":
+        actividad = input("Ingrese actividad: ")
+        lista_actividades.append(actividad)
+        seguir = input("Desea ingresar actividad ")
+    return lista_actividades
+
+def analizar_habitos(lista):
+    diccionario = {}
+    for actividad in lista:
+        if actividad not in diccionario:
+            diccionario[actividad] = 1
+        else:
+            diccionario[actividad] += 1
+    return diccionario
+
--- programa_principal.py
+++ programa_principal.py
@@ -4,4 +4,9 @@ Created on Thu Mar 12 13:52:05 2026
 
 @author: mom
 """
+import funciones_habitos
 
+lista = funciones_habitos.registar_habitos()
+diccio = funciones_habitos.analizar_habitos(lista)
+print("Resumen de actividades: ")
+print(diccio)
