d677833b16bc0186d0e11cd07465ec3e293832de
ramonotero-crack <roteromonsegur@udesa.edu.ar>
2026-03-12 13:54:11 -0300

    Primer Commit
    Creamos los dos archivos.

---
+++ funciones_habitos.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 13:50:11 2026
+
+@author: ramon
+"""
+
---
+++ programa_principal.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 13:52:05 2026
+
+@author: mom
+"""
+
